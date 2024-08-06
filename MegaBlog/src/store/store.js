5c1155f05e69314950f35f41aba0329775c3db21
= <=>
2024-08-06 14:10:01 +0530

    react cource completed

--- MegaBlog/src/store/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit"
-import authReducer from "./authSlice.js"
-const store = configureStore({
-    reducer: { 
-        auth:authReducer
-     }
-})
-
-export default store
